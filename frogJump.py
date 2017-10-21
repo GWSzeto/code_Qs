2efe255450a9e780bed92bf073b0a937faadaed1
GWszeto <GW.Stanley.Szeto@gmail.com>
2017-10-21 16:26:44 -0400

    leetcode #403 Frog Jump, created solution that exceeds the time

---
+++ frogJump.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    reached = False
+    def canLeap(self, stones, pos, lastLeap):
+        if pos == len(stones) - 1 or self.reached:
+            self.reached = True
+            return
+        next_stone = pos + 1
+        print pos, next_stone
+        diff = stones[next_stone] - stones[pos]
+
+        while diff <= lastLeap + 1:
+            if diff >= lastLeap - 1:
+                self.canLeap(stones, next_stone, diff)
+            next_stone += 1
+            if next_stone >= len(stones):
+                break
+            diff = stones[next_stone] - stones[pos]
+
+    def canCross(self, stones):
+        if stones[1] - stones[0] != 1:
+            return False
+        self.canLeap(stones, 1, 1)
+        return self.reached
+
+test_list1 = [0,1,3,5,6,8,12,17]
+test_list2 = [0,1,2,3,4,8,9,11]
+test = Solution()
+print test.canCross(test_list1)
+print test.canCross(test_list2)
